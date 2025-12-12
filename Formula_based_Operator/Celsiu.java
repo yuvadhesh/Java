ca680a02857a82d374dbd6260bcd16a7ea02427c
YUVADHESH M <yuvadheshm@gmail.com>
2025-12-12 14:04:01 +0530

    Delete Formula_based_Operator directory

--- Formula_based_Operator/Celsiu.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Celsiu{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        double celsius=sc.nextDouble();
-        Double fahrenheit=((celsius*5/9)+32);
-        System.out.print(fahrenheit);
-    }}
-
--- Formula_based_Operator/Fahrenheittocelsius.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Fahrenheittocelsius {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        double fahrenheit=sc.nextDouble();
-        double celsius=(fahrenheit-32)*5/9;
-        System.out.print(celsius);
-    }}
-
